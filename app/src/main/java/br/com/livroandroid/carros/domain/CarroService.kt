ef852a67b22ffe96251f0eab564f12ad3aacc38e
Adilson S. Utiama <adilson.utiama@gmail.com>
2017-09-05 23:49:45 -0300

    Dados de carros a partir de JSON, parser JSON

--- app/src/main/java/br/com/livroandroid/carros/domain/CarroService.kt
+++ app/src/main/java/br/com/livroandroid/carros/domain/CarroService.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import br.com.livroandroid.carros.R
 import br.com.livroandroid.carros.extensions.getText
 import br.com.livroandroid.carros.extensions.getXml
+import org.json.JSONArray
 
 /**
  * Created by Adilson on 03/09/2017.
@@ -16,17 +17,43 @@ object CarroService {
     //Busca os carros por tipo (classicos, esportivos ou luxo)
     fun getCarros(context: Context, tipo: TipoCarro): List<Carro> {
         //O Arquivo que temos que ler
-        val raw = getArquivoRaw(tipo)
+        val raw = getArquivoRawJson(tipo)
 
         //Abre o arquivo para leitura
         val resources = context.resources
         val inputStream = resources.openRawResource(raw)
         inputStream.bufferedReader().use {
             //Le o xml e cria a lista de carros
-            val xml = it.readText()
-            val carros = parseXML(xml)
+            //val xml = it.readText()
+            //val carros = parseXML(xml)
+
+            //Le o json e cria a lista de carros
+            val json = it.readText()
+            val carros = parserJSON(json)
             return carros
         }
+    }
+
+    fun parserJSON(json: String): List<Carro> {
+        val carros = mutableListOf<Carro>()
+
+        //Cria um array com este JSON
+        val array = JSONArray(json)
+
+        //Percorre cada carro (json)
+        for (i in 0..array.length() - 1) {
+            //JSON do carro
+            val jsonCarro = array.getJSONObject(i)
+            val carro = Carro()
+
+            //Le as informacoes de cadda carro
+            carro.nome = jsonCarro.optString("nome")
+            carro.desc = jsonCarro.optString("desc")
+            carro.urlFoto = jsonCarro.optString("url_foto")
+            carros.add(carro)
+        }
+        Log.d(TAG, "${carros.size} carros encontrados")
+        return carros
     }
 
     //Retorna o arquivo que temos que ler para o tipo informado
@@ -34,6 +61,12 @@ object CarroService {
         TipoCarro.classicos -> R.raw.carros_classicos
         TipoCarro.esportivos -> R.raw.carros_esportivos
         else -> R.raw.carros_luxo
+    }
+
+    fun getArquivoRawJson(tipo: TipoCarro) = when (tipo) {
+        TipoCarro.classicos -> R.raw.carros_classicos_json
+        TipoCarro.esportivos -> R.raw.carros_esportivos_json
+        else -> R.raw.carros_luxo_json
     }
 
     fun parseXML(xmlString: String): List<Carro> {
